85063e674cd3f504f6e9e5c14536ad21febb611e
Ben Kettle <theyellowtoaster@gmail.com>
2022-11-30 09:48:22 -0500

    blinking lights

--- Makefile
+++ Makefile
@@ -1,16 +1,19 @@
-build:
+main.elf: main.c vectors.s startup.s flash.ld
 	arm-none-eabi-gcc -c -ffreestanding -mthumb -g main.c
 	arm-none-eabi-gcc -c -ffreestanding -mthumb -g vectors.s
 	arm-none-eabi-gcc -c -ffreestanding -mthumb -g startup.s
 	arm-none-eabi-ld -g -T flash.ld vectors.o startup.o main.o -o main.elf
+
+main.hex: main.elf
 	arm-none-eabi-objcopy -O ihex main.elf main.hex
 	arm-none-eabi-objdump --disassemble-all --source --section-headers --demangle main.elf > main.lst
 
 recover:
 	nrfjprog --recover
 
-flash:
+flash: main.hex
 	nrfjprog -f NRF52 --program main.hex --verify --chiperase
+	nrfjprog --reset
 
 
 clean:
@@ -24,5 +27,5 @@ clean:
 debug-server:
 	JLinkGDBServer -device NRF52840_XXAA -if swd -port 2331
 
-debug:
+debug: main.elf
 	arm-none-eabi-gdb -x debug.gdb main.elf
--- main.c
+++ main.c
@@ -9,10 +9,41 @@ int y = 3;
 #define GPIO_0_DIR ((uint32_t *) (GPIO_BASE + DIR_OFF))
 #define GPIO_0_OUT ((uint32_t *) (GPIO_BASE + OUT_OFF))
 
+// LEDs are mapped to bits 13-16 of GPIO_0_OUT
+
+void enableLed(int ledNum) {
+	// led numbers are zero-indexed
+	// active-low
+	*GPIO_0_OUT &= ~(1 << (13 + (ledNum)));	
+}
+
+void disableLed(int ledNum) {
+	// led numbers are zero-indexed
+	*GPIO_0_OUT |= (1 << (13 + (ledNum)));	
+}
+
 int main() {
 	x = y + y;	
 	*GPIO_0_DIR = 0x1E000;
-	*GPIO_0_OUT = 0x18000;	
+	*GPIO_0_OUT = 0x1E000; // disable all LEDS
+	// enableLed(1);
+	// enableLed(2);
+	// enableLed(3);
+	enableLed(0);
+	disableLed(2);
 	while (1) {
+		int i=0;
+		while (i < (1<<20)) {
+			++i;
+		}
+		i = 0;
+		disableLed(0);
+		enableLed(2);
+		while (i < (1<<20)) {
+			++i;
+		}
+		i = 0;
+		enableLed(0);
+		disableLed(2);
 	}
 }
